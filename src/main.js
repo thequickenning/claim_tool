a09a7c0aa13d0f60f11fbbe3255cf60696125418
Anthony Rizzo <rizzo0917@gmail.come>
2018-05-26 09:22:49 -0400

    Chore: project cleanup replace require statements in favor of es6 import syntax use more strict eslint rules

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueMeta from 'vue-meta'
+import Vuetify from 'vuetify'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+import routes from './routes.js'
+import store from './store.js'
+
+import '../node_modules/vuetify/dist/vuetify.min.css'
+
+Vue.use(VueRouter)
+Vue.use(VueMeta)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#000'
+  }
+})
+
+const router = new VueRouter({ routes: routes, mode: 'history' })
 
 /* eslint-disable no-new */
 new Vue({
+  store: store,
+  router,
   el: '#app',
-  components: { App },
-  template: '<App/>',
-});
+  render: h => h(App),
+  components: { App }
+})
